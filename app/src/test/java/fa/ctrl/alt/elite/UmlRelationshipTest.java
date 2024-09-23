30aa1abaf82852b20947b8bd6afc5cce4042d4b8
Sydney Norgaard <sydney.norgaard@gmail.com>
2024-09-22 23:17:39 -0400

    Update UmlRelationshipTest.java
    File updated

--- app/src/test/java/fa/ctrl/alt/elite/UmlRelationshipTest.java
+++ app/src/test/java/fa/ctrl/alt/elite/UmlRelationshipTest.java
@@ -1,32 +1,92 @@
 package fa.ctrl.alt.elite;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * A test class that checks the functionality of UmlRelationship.
+ */
 public class UmlRelationshipTest {
-
+	
+	/** A UmlRelationship object that will be tested on. */
+	private UmlRelationship relationship;
+	
+	/**
+     * Creates an instance of a UmlRelationship object to be used in tests.
+     */
+    @BeforeEach
+    public void setUp() {
+    	relationship = new UmlRelationship("ClassA", "ClassB");
+    }
+	
+	/**
+	 * Test that the object was initialized correctly.
+	 */
     @Test
-    public void testConstructorAndGetters() {
-        UmlRelationship relationship = new UmlRelationship("ClassA", "ClassB");
+    @DisplayName ("UmlRelationship: The UmlRelationship object was created properly")
+    public void testUmlRelationship() {
         assertEquals("ClassA", relationship.getSource());
         assertEquals("ClassB", relationship.getDestination());
     }
 
+    /**
+     * Test that getSource returns the source of the relationship.
+     */
     @Test
+    @DisplayName ("GetSource: Retrive the source of the relationship")
+    public void testGetSource() {
+        assertEquals("ClassA", relationship.getSource());
+    }
+    
+    /**
+     * Test that getDestination returns the destination of the relationship.
+     */
+    @Test
+    @DisplayName ("GetDestinatio: Retrive the destination of the relationship")
+    public void testGetDestination() {
+        assertEquals("ClassB", relationship.getDestination());
+    }
+    
+    /**
+     * Test that two relationships that are the same equal each other.
+     */
+    @Test
+    @DisplayName ("Equals: Test that two relationships that are the same equal each other")
     public void testEquality() {
-        UmlRelationship relationship1 = new UmlRelationship("ClassA", "ClassB");
         UmlRelationship relationship2 = new UmlRelationship("ClassA", "ClassB");
-        UmlRelationship relationship3 = new UmlRelationship("ClassB", "ClassA");
-        
-        assertEquals(relationship1, relationship2);  // Same source and destination
-        assertNotEquals(relationship1, relationship3);  // Different source and destination
+        assertEquals(relationship, relationship2);
+    }
+    
+    /**
+     * Test that two relationships that are not the same do not equal each other, should fail.
+     */
+    @Test
+    @DisplayName ("Equals: Test that two relationships, that are not the same, are not equal, failure test")
+    public void testNotEquality() {
+    	UmlRelationship relationship2 = new UmlRelationship("ClassB", "ClassA");
+    	assertNotEquals(relationship, relationship2);
     }
 
+    
+    /**
+     * Test that the same objects have the same hash code.
+     */
     @Test
+    @DisplayName ("HashCode: Test that the same objects have the same hash code")
     public void testHashCode() {
-        UmlRelationship relationship1 = new UmlRelationship("ClassA", "ClassB");
         UmlRelationship relationship2 = new UmlRelationship("ClassA", "ClassB");
-        
-        assertEquals(relationship1.hashCode(), relationship2.hashCode());  // Same hash code for equal objects
+        assertEquals(relationship.hashCode(), relationship2.hashCode());
+    }
+    
+    /**
+     * Test that different objects have different hash code, should fail.
+     */
+    @Test
+    @DisplayName ("HashCode: Test that different objects different hash code, failure test")
+    public void testHashCodeDifferent() {
+        UmlRelationship relationship2 = new UmlRelationship("ClassB", "ClassA");
+        assertNotEquals(relationship.hashCode(), relationship2.hashCode());
     }
 }
