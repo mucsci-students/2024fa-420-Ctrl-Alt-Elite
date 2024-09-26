22332e714ef421ef78a4333b3ced4f47e7ca557f
Jordan Rios <Jordanrios130@gmail.com>
2024-09-25 22:37:22 -0400

    changed utils

--- app/src/main/java/JsonUtils.java
+++ app/src/main/java/JsonUtils.java
@@ -1,6 +1,7 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.FileReader;
