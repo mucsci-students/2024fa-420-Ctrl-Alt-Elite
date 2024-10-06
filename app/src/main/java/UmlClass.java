e5c132f94e75f68199da99e86f2b9c472b13c341
Sydney Norgaard <sydney.norgaard@gmail.com>
2024-10-05 20:12:54 -0400

    Adjusted Comments
    Polished and added comments to UMLClass

--- app/src/main/java/UmlClass.java
+++ app/src/main/java/UmlClass.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 
-//TODO check comments
 /**
  * Represents a UML class with a name and a list of methods.
  */
@@ -11,7 +10,7 @@ public class UmlClass {
     /**
      * Constructs a new UmlClass with the specified name.
      * 
-     * @param name the name of the UML class
+     * @param name The name of the UML class.
      */
     public UmlClass(String name) {
         this.name = name;
@@ -21,7 +20,7 @@ public class UmlClass {
     /**
      * Gets the name of the UML class.
      * 
-     * @return the name of the class
+     * @return The name of the class.
      */
     public String getName() {
         return name;
@@ -30,15 +29,14 @@ public class UmlClass {
     /**
      * Sets a new name for the UML class.
      * 
-     * @param name the new name to be set for the class
+     * @param name The new name to be set for the class.
      */
     public void setName(String name) {
         this.name = name;
     }
 
-
     /**
-     * Repersents a method that has a list of parameters.
+     * Repersents a method with a list of parameters.
      */
     public class Method {
         /** The name of the method. */
@@ -57,7 +55,7 @@ public class UmlClass {
         }
 
         /**
-         * Retrieves the name of the method.
+         * Returns the name of the method.
          * 
          * @return The name of the method.
          */
@@ -75,7 +73,7 @@ public class UmlClass {
         }
 
         /**
-         * Retrieves the list of parameters.
+         * Returns the list of parameters.
          * 
          * @return The list of parameters.
          */
@@ -92,7 +90,57 @@ public class UmlClass {
             this.parameters = parameters;
         }
 
+        /**
+         * Compares this Method with another object for equality.
+         * Two Methods are considered equal if they have the same name
+         * and parameter list.
+         * 
+         * @param obj The object to be compared.
+         * @return {@code true} if the objects are equal, {@code false} otherwise.
+         */
+        @Override
+	    public boolean equals(Object obj) {
+            if (this == obj) { // If both references point to the same object, they are equal.
+                return true;
+            }
+            if (obj == null) { // If the other object is null, they are not equal.
+                return false;
+            }
+            if (getClass() != obj.getClass()) { // If the classes are different, they are not equal.
+                return false;
+            }
+            
+            // Cast the object to UmlRelationship for comparison.
+            Method other = (Method) obj;
+            
+            // Compare the source fields for equality.
+            if (name == null) {
+                if (other.name != null) {
+                    return false;
+                }
+            } else if (!name.equals(other.name)) {
+                return false;
+            }
+            
+            // Compare the destination fields for equality.
+            if (parameters == null) {
+                if (other.parameters != null) {
+                    return false;
+                }
+            } else if (!parameters.equals(other.parameters)) {
+                return false;
+            }
+            
+            // If both source and destination are equal, the objects are equal.
+            return true;
+        }
 
+        /**
+         * Generates a hash code for the Method object.
+         * The hash code is computed based on the name and parameter list.
+         * 
+         * @return An integer representing the hash code of the object.
+         */
         @Override
 	    public int hashCode() {
 		    final int prime = 31;
@@ -102,44 +150,9 @@ public class UmlClass {
 		    return result;
 	    }
 
-        @Override
-	    public boolean equals(Object obj) {
-            if (this == obj) { // If both references point to the same object, they are equal
-                return true;
-            }
-            if (obj == null) { // If the other object is null, they are not equal
-                return false;
-            }
-            if (getClass() != obj.getClass()) { // If the classes are different, they are not equal
-                return false;
-            }
-            
-            // Cast the object to UmlRelationship for comparison
-            Method other = (Method) obj;
-            
-            // Compare the source fields for equality
-            if (name == null) {
-                if (other.name != null) {
-                    return false;
-                }
-            } else if (!name.equals(other.name)) {
-                return false;
-            }
-            
-            // Compare the destination fields for equality
-            if (parameters == null) {
-                if (other.parameters != null) {
-                    return false;
-                }
-            } else if (!parameters.equals(other.parameters)) {
-                return false;
-            }
-            
-            // If both source and destination are equal, the objects are equal
-            return true;
-        }
-
-        //TODO
+        /**
+         * Generates a string repersentaion of the Method object.
+         */
         @Override
         public String toString() {
             String string = "\tMethod: " + name;
@@ -154,27 +167,26 @@ public class UmlClass {
             string = string.concat(")\n");
             return string;
         }
-
     }
-
 
     //TODO methods cannot share names
     /**
      * Adds a new method to the UML class.
      * 
-     * @param methodName the name of the method to add
-     * @param parameters the list of parameters for the method
-     * @return {@code true} if the method was added, {@code false} if the method already exists
+     * @param methodName The name of the method to add.
+     * @param parameters The list of parameters for the method.
+     * @return {@code true} if the method was added, {@code false} if the method already exists.
      */
     public boolean addMethod(String methodName, ArrayList<String> parameters) {
-        //create a new method object
-        //then add that object to the set of methods
+        // Loop through the methods to see if a method
+        //  with methodName already exists.
         for (Method method : methods) {
             if (method.getName().equals(methodName)) {
                 return false;
             }
         }
        
+        // Add the new method.
         Method newMethod = new Method(methodName, parameters);
         return methods.add(newMethod);
     }
@@ -183,11 +195,12 @@ public class UmlClass {
     /**
      * Deletes a method from the UML class.
      * 
-     * @param methodName the name of the method to delete
-     * @return {@code true} if the method was removed, {@code false} if the method was not found
+     * @param methodName The name of the method to delete.
+     * @return {@code true} if the method was removed, {@code false} if the method was not found.
      */
     public boolean deleteMethod(String methodName) {
-        //remove the method object, by looking for its name, from the set of methods
+        // Loop through the methods to find the method
+        //  that's named "methodName" and remove it.
         for (Method method : methods) {
             if (method.getName().equals(methodName)) {
                 return methods.remove(method);
@@ -201,37 +214,36 @@ public class UmlClass {
     /**
      * Renames an existing method in the UML class.
      * 
-     * @param oldName the current name of the method to rename
-     * @param newName the new name of the method
+     * @param oldName The current name of the method to rename.
+     * @param newName The new name of the method.
      * @return {@code true} if the method was successfully renamed, {@code false} if the old name was not found 
-     *         or if the new name already exists
+     *         or if the new name already exists.
      */
     public boolean renameMethod(String oldName, String newName) {
-        /** If there are no methods to rename, return false */
+        // If there are no methods to rename, return false
         if (oldName == null || newName == null || methods.isEmpty()) {
             return false;
         }
         
-        /** The index of the method to be renamed. */
-        int index = 0;
-
+        // Loop through the methods and check if a method 
+        //  with the new name is already present.
         for (Method method : methods) {
-            String name = method.getName();
-            if (name.equals(newName)) {
+            if (method.getName().equals(newName)) {
                 return false;
-            }
-            
-            if (name.equals(oldName)) {
-                index = methods.indexOf(method);
+            } 
+        }
+
+        // Loop through the methods and find the method with 
+        //  the old name an replace it with the new name.
+        for (Method method : methods) {
+            if (method.getName().equals(oldName)) {
+                method.setName(newName);
             }
         }
-        
-        methods.get(index).setName(newName);
+
         return true;
     }
     
-
-   //TODO
     /**
      * Returns a string representation of the UML class, including its name and methods, 
      *  along with their parameters.
@@ -240,7 +252,6 @@ public class UmlClass {
      */
     @Override
     public String toString() {
-        //call the method toString on all methods in the set
         String string = new String();
         string = string.concat("Class: " + name + "\n");
         for (Method method : methods) {
