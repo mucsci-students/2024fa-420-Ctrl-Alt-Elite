523abadd3bab13d2ce3fcd952ecf5fc0595e9ae0
Jordan Rios <Jordanrios130@gmail.com>
2024-10-14 18:59:39 -0400

    updated GUI.java file with new version from GUI branch

--- app/src/test/java/UmlClassTest.java
+++ app/src/test/java/UmlClassTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 /**
  * A test class that checks the functionality of UnlClass.
  */
