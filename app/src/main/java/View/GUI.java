a1f02504aeb57b4925b61d4895a9c898be3b7c7c
Jordan Rios <Jordanrios130@gmail.com>
2024-10-24 21:26:44 -0400

    Set the native look and feel in GUI.java main method.

--- app/src/main/java/View/GUI.java
+++ app/src/main/java/View/GUI.java
@@ -2,12 +2,20 @@ package View;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 import Controller.UmlGuiController;
 
 public class GUI extends JFrame {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            try {
+                // Set the native look and feel
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace(); // Handle exceptions appropriately
+            }
+
             UmlGuiController gui = new UmlGuiController();
             gui.setVisible(true);
         });
